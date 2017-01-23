a8e6e5eba64e41988c4ff5c6436d0fb8d2d31a68
Maddy Deason <mdeason89@gmail.com>
2017-01-23 12:04:48 -0500

    add js code

--- javascript-drumkit/js/script.js
+++ javascript-drumkit/js/script.js
@@ -1,1 +1,18 @@
-//js goes here
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return; //stop function running
+    audio.currentTime = 0; //rewind to start of audio file
+    audio.play();
+    key.classList.add('playing'); //same as key.addClass('playing') in JQuery
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; //skip if not transform
+    this.classList.remove('playing'); //same as key.removeClass('playing') in JQuery
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
